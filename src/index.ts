f3ec6651f030e2edcfa91282c6d66f3b2fa10d03
JorgeADnro <jorgelamanrique@gmail.com>
2024-06-21 20:21:35 -0600

    Creación del proyecto

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import SwaggerUi  from "swagger-ui-express";
+
+class Server {
+    // * Crear la instancia global de nuestra aplicación
+    public app: Application;
+    
+    // ? Generar el constructor
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+    
+    // * Generar un método para la configuración
+    private config(): void {
+
+        // * Configuración del puerto para el server.
+        this.app.set('port', process.env.PORT || 3000);
+
+        // * Mostrar las peticiones en consola
+        this.app.use(morgan("dev"));
+
+        // * Uso de CORS(Cross Origin)
+        this.app.use(cors);
+
+        // * Generar restricciones a la API
+        this.app.use(express.json);
+        this.app.use(express.urlencoded({extended: false}));
+    }
+    
+    // TODO: Generar un método para la configuración de rutas
+    private routes():void {
+        throw new Error('Not implemented');
+    }
+    
+    // * Generar un método para inicializar el servicio
+    start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port ', this.app.get('port'));
+        });
+    }
+
+}
+
+const server = new Server();
+server.start();
